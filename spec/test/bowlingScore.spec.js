a42d68e982f6dd08a1aea6a633313aa1a906d720
izebb <you@example.com>
2016-08-29 00:12:57 +0100

    Test cases for bowling game

--- spec/test/bowlingScore.spec.js
+++ spec/test/bowlingScore.spec.js
@@ -11,6 +11,40 @@ describe('Bowling game', () => {
           expect(bowling.getFrame().length).toBe(10);
     });
 
+    // 2.
+    it("should handle a first strike", function() {
+
+    });
+
+    // 3.
+    it("should handle a spare", function() {
+
+
+    });
+
+    // 3.
+    it("should handle a  2 strikes and 1 spare", function() {
+
+    });
+
+    // 4.
+    it("should handle all missed ball played", function() {
+
+    });
+
+    // 5.
+    it("should handle all spare scores", function() {
+
+    });
+
+    // 6.
+    it("should handle last two sparse and remaining strike", function() {
+
+    });
+
+    // 7.
+    it("should handle a perfect game", function() {
     
+    });
 
 })
